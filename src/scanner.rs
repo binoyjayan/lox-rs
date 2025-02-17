90f297581b11062515732229b281a6cff8d5d085
Binoy Jayan <binoyjayan@gmail.com>
2025-02-17 10:40:22 -0800

    scan multicharacter tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,6 @@
 use miette::{Error, LabeledSpan};
 
-use crate::token::Token;
+use crate::token::{Token, TokenKind};
 
 pub struct Scanner<'de> {
     whole: &'de str,
@@ -24,35 +24,112 @@ impl<'de> Iterator for Scanner<'de> {
 
     /// Returns the next token after consuming the input.
     fn next(&mut self) -> Option<Self::Item> {
-        let mut chars = self.rest.chars();
-        let c = chars.next()?;
-        self.rest = chars.as_str();
-        self.byte += c.len_utf8();
+        loop {
+            let mut chars = self.rest.chars();
+            let c = chars.next()?;
+            let char_str = &self.rest[..c.len_utf8()];
+            let c_onwards = self.rest;
+            self.rest = chars.as_str();
+            self.byte += c.len_utf8();
 
-        match c {
-            '(' => return Some(Ok(Token::LeftParen)),
-            ')' => return Some(Ok(Token::RightParen)),
-            '{' => return Some(Ok(Token::LeftBrace)),
-            '}' => return Some(Ok(Token::RightBrace)),
-            ',' => return Some(Ok(Token::Comma)),
-            '.' => return Some(Ok(Token::Dot)),
-            '-' => return Some(Ok(Token::Minus)),
-            '+' => return Some(Ok(Token::Plus)),
-            ';' => return Some(Ok(Token::Semicolon)),
-            '*' => return Some(Ok(Token::Star)),
-            '"' => {}
-            _ => {
-                return Some(Err(miette::miette!(
-                    labels = vec![LabeledSpan::at(
-                        self.byte - c.len_utf8()..self.byte,
-                        "this character"
-                    )],
-                    "Unexpected token '{c}' in input",
-                )
-                .with_source_code(self.whole.to_string())))
+            enum Started {
+                IfEqualElse(TokenKind, TokenKind),
+                String,
+                Number,
+                Ident,
             }
+
+            // Single character tokens
+            let char_token = |kind: TokenKind| {
+                Some(Ok(Token {
+                    literal: char_str,
+                    kind,
+                }))
+            };
+            let new_token = |kind: TokenKind, literal: &'de str| Some(Ok(Token { literal, kind }));
+
+            let started = match c {
+                '(' => return char_token(TokenKind::LeftParen),
+                ')' => return char_token(TokenKind::RightParen),
+                '{' => return char_token(TokenKind::LeftBrace),
+                '}' => return char_token(TokenKind::RightBrace),
+                ',' => return char_token(TokenKind::Comma),
+                '.' => return char_token(TokenKind::Dot),
+                '-' => return char_token(TokenKind::Minus),
+                '+' => return char_token(TokenKind::Plus),
+                ';' => return char_token(TokenKind::Semicolon),
+                '*' => return char_token(TokenKind::Star),
+                '/' => return char_token(TokenKind::Slash),
+                '!' => Started::IfEqualElse(TokenKind::BangEqual, TokenKind::Bang),
+                '=' => Started::IfEqualElse(TokenKind::EqualEqual, TokenKind::Equal),
+                '<' => Started::IfEqualElse(TokenKind::LessEqual, TokenKind::Less),
+                '>' => Started::IfEqualElse(TokenKind::GreaterEqual, TokenKind::Greater),
+                '"' => Started::String,
+                '0'..='9' => Started::Number,
+                'a'..='z' | 'A'..='Z' | '_' => Started::Ident,
+                c if c.is_whitespace() => continue,
+                _ => {
+                    return Some(Err(miette::miette!(
+                        labels = vec![LabeledSpan::at(
+                            self.byte - c.len_utf8()..self.byte,
+                            "this character"
+                        )],
+                        "Unexpected token '{c}' in input",
+                    )
+                    .with_source_code(self.whole.to_string())))
+                }
+            };
+
+            match started {
+                Started::IfEqualElse(first, second) => {
+                    // Remove whitespace before checking for '='
+                    self.rest = self.rest.trim_start();
+                    // Characters read from rest including trimmed whitespace excluding current character
+                    let trimmed = c_onwards.len() - self.rest.len() - 1;
+                    self.byte += trimmed;
+                    if self.rest.trim_start().starts_with('=') {
+                        // 1 byte is for the '=' character
+                        let span = &c_onwards[..c.len_utf8() + trimmed + 1];
+                        self.rest = &self.rest[1..];
+                        self.byte += 1;
+                        return new_token(first, span);
+                    }
+                    return char_token(second);
+                }
+                Started::String => todo!(),
+                Started::Number => {
+                    let first_non_digit = c_onwards
+                        .find(|c| !matches!(c, '.' | '0'..='9'))
+                        .unwrap_or_else(|| c_onwards.len());
+                    let mut literal = &c_onwards[..first_non_digit];
+                    let mut dotted = literal.splitn(3, '.');
+                    // If there are two dots (i.e. if there are three parts of the split)
+                    if let (Some(one), Some(two), Some(_)) =
+                        (dotted.next(), dotted.next(), dotted.next())
+                    {
+                        // Use only the first two parts
+                        literal = &literal[..one.len() + 1 + two.len()];
+                    }
+                    let extra_bytes = literal.len() - c.len_utf8();
+                    self.byte += extra_bytes;
+                    self.rest = &self.rest[extra_bytes..];
+                    let num = match literal.parse() {
+                        Ok(num) => num,
+                        Err(e) => {
+                            return Some(Err(miette::miette!(
+                                labels = vec![LabeledSpan::at(
+                                    self.byte - literal.len()..self.byte,
+                                    "this numeric literal"
+                                )],
+                                "{e}",
+                            )
+                            .with_source_code(self.whole.to_string())))
+                        }
+                    };
+                    return new_token(TokenKind::Number(num), literal);
+                }
+                Started::Ident => todo!(),
+            };
         }
-        self.byte += c.len_utf8();
-        todo!()
     }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,5 +1,11 @@
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Token<'de> {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token<'de> {
+    pub literal: &'de str,
+    pub kind: TokenKind,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -10,31 +16,82 @@ pub enum Token<'de> {
     Plus,
     Semicolon,
     Star,
-    String(&'de str),
-    EOF,
+    Bang,
+    Equal,
+    BangEqual,
+    EqualEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Slash,
+    String,
+    Ident,
+    Number(f64),
+    And,
+    Class,
+    Else,
+    False,
+    For,
+    Fun,
+    If,
+    Nil,
+    Or,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
 }
 
 impl std::fmt::Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Token::LeftParen => write!(f, "LEFT_PAREN ( null"),
-            Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
-            Token::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
-            Token::RightBrace => write!(f, "RIGHT_BRACE }} null"),
-            Token::Comma => write!(f, "COMMA , null"),
-            Token::Dot => write!(f, "DOT . null"),
-            Token::Minus => write!(f, "MINUS - null"),
-            Token::Plus => write!(f, "PLUS + null"),
-            Token::Semicolon => write!(f, "SEMICOLON ; null"),
-            Token::Star => write!(f, "STAR * null"),
-            Token::String(s) => write!(f, "STRING \"{}\" null", s),
-            Token::EOF => write!(f, "EOF"),
+        let literal = self.literal;
+        match self.kind {
+            TokenKind::LeftParen => write!(f, "LEFT_PAREN {literal} null"),
+            TokenKind::RightParen => write!(f, "RIGHT_PAREN {literal} null"),
+            TokenKind::LeftBrace => write!(f, "LEFT_BRACE {literal} null"),
+            TokenKind::RightBrace => write!(f, "RIGHT_BRACE {literal} null"),
+            TokenKind::Comma => write!(f, "COMMA {literal} null"),
+            TokenKind::Dot => write!(f, "DOT {literal} null"),
+            TokenKind::Minus => write!(f, "MINUS {literal} null"),
+            TokenKind::Plus => write!(f, "PLUS {literal} null"),
+            TokenKind::Semicolon => write!(f, "SEMICOLON {literal} null"),
+            TokenKind::Star => write!(f, "STAR {literal} null"),
+            TokenKind::Bang => write!(f, "BANG {literal} null"),
+            TokenKind::Equal => write!(f, "EQUAL {literal} null"),
+            TokenKind::BangEqual => write!(f, "BANG_EQUAL {literal} null"),
+            TokenKind::EqualEqual => write!(f, "EQUAL_EQUAL {literal} null"),
+            TokenKind::Less => write!(f, "LESS {literal} null"),
+            TokenKind::LessEqual => write!(f, "LESS_EQUAL {literal} null"),
+            TokenKind::Greater => write!(f, "GREATER {literal} null"),
+            TokenKind::GreaterEqual => write!(f, "GREATER_EQUAL {literal} null"),
+            TokenKind::Slash => write!(f, "SLASH {literal} null"),
+            TokenKind::String => write!(f, "STRING {literal} {}", Token::unescape(literal)),
+            TokenKind::Ident => write!(f, "IDENTIFIER {literal} null"),
+            TokenKind::Number(n) => write!(f, "NUMBER {literal} {n}"),
+            TokenKind::And => write!(f, "AND {literal} null"),
+            TokenKind::Class => write!(f, "CLASS {literal} null"),
+            TokenKind::Else => write!(f, "ELSE {literal} null"),
+            TokenKind::False => write!(f, "FALSE {literal} null"),
+            TokenKind::For => write!(f, "FOR {literal} null"),
+            TokenKind::Fun => write!(f, "FUN {literal} null"),
+            TokenKind::If => write!(f, "IF {literal} null"),
+            TokenKind::Nil => write!(f, "NIL {literal} null"),
+            TokenKind::Or => write!(f, "OR {literal} null"),
+            TokenKind::Return => write!(f, "RETURN {literal} null"),
+            TokenKind::Super => write!(f, "SUPER {literal} null"),
+            TokenKind::This => write!(f, "THIS {literal} null"),
+            TokenKind::True => write!(f, "TRUE {literal} null"),
+            TokenKind::Var => write!(f, "VAR {literal} null"),
+            TokenKind::While => write!(f, "WHILE {literal} null"),
         }
     }
 }
 
 impl Token<'_> {
-    fn _unescape<'de>(_s: &'de str) -> std::borrow::Cow<'de, str> {
+    fn unescape<'de>(_s: &'de str) -> std::borrow::Cow<'de, str> {
         todo!()
     }
 }
